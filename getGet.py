d077f87ab58ac933ce933968df7c74ab5ae56f68
Morten Hansen (MLM) <MLM@kmd.dk>
2021-11-12 11:09:06 +0100

    a

--- getGet.py
+++ getGet.py
@@ -34,21 +34,30 @@ data_sold = pd.read_csv("boliga_data_sold_best.csv")
 
 latitude = []
 longitude = []
-'''
 for i in tqdm(range(len(being_sold))):
     address = being_sold["post_nummer"][i]
+    address = address.split(',')
+    for s in address[-1].split():
+         if s.isdigit() and len(s) == 4:
+            address = address[0] + ", "+s +" Denmark"
+    else: pass
     request = json.loads(requests.get("https://api.opencagedata.com/geocode/v1/json?q="+address+"%2Cdanmark&key=03c48dae07364cabb7f121d8c1519492&no_annotations=1&language=en").text)
     latitude.append(request['results'][0]['geometry']['lat'])
     longitude.append(request['results'][0]['geometry']['lng'])
 being_sold.insert(loc=2, column='latitude', value=latitude)
 being_sold.insert(loc=3, column='longitude', value=longitude)
-being_sold.to_csv("boliga_data_being_sold_best_updated.csv",index=False)
-'''
+#being_sold.to_csv("boliga_data_being_sold_best_updated.csv",index=False)
+
 latitude.clear()
 longitude.clear()
 
 for i in tqdm(range(len(data_sold))):
     address = data_sold["post_nummer"][i]
+    address = address.split(',')
+    for s in address[-1].split():
+         if s.isdigit() and len(s) == 4:
+            address = address[0] + ", "+s +" Denmark"
+    else: pass
     try:
         request = json.loads(requests.get("https://api.opencagedata.com/geocode/v1/json?q="+address+"%2Cdanmark&key=03c48dae07364cabb7f121d8c1519492&no_annotations=1&language=en").text)
         latitude.append(request['results'][0]['geometry']['lat'])
@@ -58,4 +67,4 @@ for i in tqdm(range(len(data_sold))):
         longitude.append(" ")
 data_sold.insert(loc=2, column='latitude', value=latitude)
 data_sold.insert(loc=3, column='longitude', value=longitude)
-data_sold.to_csv("boliga_data_sold_best_updated.csv",index=False)
+#data_sold.to_csv("boliga_data_sold_best_updated.csv",index=False)
